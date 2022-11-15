d0cc9b571efe33035e16c86ad8f841f43bedb123
Your Name <you@example.com>
2022-11-15 08:59:28 -0500

    introduccion react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const bootcamps=[
+        {
+           id:1, 
+           name:'messi',
+           average_rating: 10
+        },
+        {
+            id:2, 
+            name:'cr7',
+            average_rating: 7  
+        }
+    ]
+return(
+    
+    <div>
+        <h1>
+        listado de bootcamps
+    </h1>
+    <ul>
+    {
+    bootcamps.map((bootcamps, index) =>(
+        <li key={index} >
+            {
+            bootcamps.name
+        }
+        </li>
+    ))    
+    }
+    </ul>
     </div>
-  );
+)
 }
 
-export default App;
+export default App
